865598c5086ef3b73fa6a775d85e199c3378f289
Adam Eckert <clyde480@gmail.com>
2021-09-03 17:43:42 -0500

    updated footer, header gifs, selected projects

--- imageselect.js
+++ imageselect.js
@@ -1,9 +1,10 @@
 GifArray = new Array();
 GifArray[0]= 'tff.gif';
 GifArray[1] = 'sb.gif';
+GifArray[2] = 'dd.gif';
 
 function getRandomGif() {
-    var num = Math.floor( Math.random() * 2);
+    var num = Math.floor( Math.random() * 3);
     var img = GifArray[num];
     document.getElementById("headergif").innerHTML = ('<img id="hgif" src="' + '/images/' + img + '">');
 }
