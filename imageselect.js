98a4add9c474ed3e5b9ec5751483ee47a389b167
Adam Eckert <clyde480@gmail.com>
2021-09-08 22:17:51 -0500

    Color Phase and partial Death Dweller

--- imageselect.js
+++ imageselect.js
@@ -2,9 +2,10 @@ GifArray = new Array();
 GifArray[0]= 'tff.gif';
 GifArray[1] = 'sb.gif';
 GifArray[2] = 'dd.gif';
+GifArray[3] = 'cph.gif';
 
 function getRandomGif() {
-    var num = Math.floor( Math.random() * 3);
+    var num = Math.floor( Math.random() * 4);
     var img = GifArray[num];
     document.getElementById("headergif").innerHTML = ('<img id="hgif" src="' + '/images/' + img + '">');
 }
