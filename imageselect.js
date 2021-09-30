6f766f6c0e0b35c5e02566dc40af9de99c08ec8d
Adam Eckert <clyde480@gmail.com>
2021-09-30 12:44:28 -0500

    Added Solos & Pairs gif

--- imageselect.js
+++ imageselect.js
@@ -3,9 +3,10 @@ GifArray[0]= 'tff.gif';
 GifArray[1] = 'sb.gif';
 GifArray[2] = 'dd.gif';
 GifArray[3] = 'cph.gif';
+GifArray[4] = 'sap.gif';
 
 function getRandomGif() {
-    var num = Math.floor( Math.random() * 4);
+    var num = Math.floor( Math.random() * 5);
     var img = GifArray[num];
     document.getElementById("headergif").innerHTML = ('<img id="hgif" src="' + '/images/' + img + '">');
 }
