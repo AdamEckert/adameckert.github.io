a00c9cab72b60f424e1de89922fb3b5681ecf7e7
Adam Eckert <clyde480@gmail.com>
2022-08-30 22:13:40 -0500

    Added dodger, edited many pages and resume

--- imageselect.js
+++ imageselect.js
@@ -4,15 +4,17 @@ GifArray[1] = 'sb.gif';
 GifArray[2] = 'dd.gif';
 GifArray[3] = 'cph.gif';
 GifArray[4] = 'sap.gif';
+GifArray[5] = 'dodger.gif';
 PageArray = new Array();
 PageArray[0]= 'finalfrontiersman.html';
 PageArray[1] = 'spacebubby.html';
 PageArray[2] = 'deathdweller.html';
 PageArray[3] = 'colorphase.html';
 PageArray[4] = 'solosandpairs.html';
+PageArray[5] = 'dodger.html';
 
 function getRandomGif() {
-    var num = Math.floor( Math.random() * 5);
+    var num = Math.floor( Math.random() * 6);
     var img = GifArray[num];
     var pg = PageArray[num];
     document.getElementById("headergif").innerHTML = ('<a href="/projects/'+pg+'"><img id="hgif" src="' + '/images/' + img + '">');
